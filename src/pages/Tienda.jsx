f0574c771bded8951f60eee90af5eb42c141b86f
RicardoAyala2003 <92557271+RicardoAyala2003@users.noreply.github.com>
2025-04-03 17:41:12 -0600

    tienda!!!

--- src/pages/Tienda.jsx
+++ src/pages/Tienda.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router";
 export const Tienda = () => {
     return (
         <section className="container">
-        <h1 className="title">Tienda</h1>    
+        <h1 className="title">Tienda!!!!!!!!!!!</h1>    
         </section>
     )
 }
